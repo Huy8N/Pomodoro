b8e41758f917f1af3be6630d8e296c2d35af242d
Huy Nguyen <huyng05155@gmail.com>
2025-05-15 21:57:11 -0400

    Title and initial timer

---
+++ react-pomodoro/src/Pomodoro.jsx
@@ -0,0 +1,14 @@
+function Pomodoro() {
+    return (
+        <> 
+        <div className="pomodoro-container">
+                <h1>Pomodoro+</h1> 
+                <div className="liveTimer-container">
+                    <p>10:42</p>
+                </div>   
+        </div>       
+        </>
+    )
+}
+
+export default Pomodoro
