30c8d1d7ba00508b4c09d30c3e0b9292a5dae711
Huy Nguyen <huyng05155@gmail.com>
2025-06-03 01:27:55 -0400

    WIP: Implementating PKCE Auth

--- react-pomodoro/src/useSpotifyAuth.js
+++ react-pomodoro/src/useSpotifyAuth.js
@@ -1,24 +1,35 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useCallback} from 'react';
+import axios from 'axios';
 
-//function to retrie spotify info
-const useSpotifyAuth = () => {
-    // Access token
-    const [accessToken, setAccessToken] = useState(null);
-    // If user is authenticated
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+//get env varibles
+const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const SPOTIFY_REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
 
-    const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-    const REDIRECT_URL = import.meta.env.VITE_SPOTIFY_REDIRECT_URL;
-    const SCOPES = 'user-modify-playback-state user-read-playback-state streaming';
+//endpoints
+const AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+//What to have access to
+const SCOPES = 'streaming user-modify-playback-state user-read-currently-playing';
 
-    const getAuthURL = () => {
-        const params = new URLSearchParams({
-            client_id: CLIENT_ID,
-            reponse_type: 'token',
-            redirect_url: REDIRECT_URL,
-            scope: SCOPES,
-        })
-        return `https://accounts.spotify.com/authorize?${params}`;
-    }
-
+//PKCE helper function from spotify doc
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 }
+
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+
+
